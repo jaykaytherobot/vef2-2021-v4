0d542dcdb8daae01bb11fd37fdb8276c60febe70
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-03-11 10:51:14 +0000

    added optional cache, depends on if REDIS_URL is in .env

--- src/cache.js
+++ src/cache.js
@@ -5,27 +5,37 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export let getEarthquakes;
+export let setEarthquakes;
+
+
 const {
   REDIS_URL: redis_url 
 } = process.env;
 
-if(!redis_url) {
-  console.error("Vantar redis url");
-  process.exit(1);
+try {
+  if (!redis_url) {
+    throw Error('No redis url');
+  }
+
+  const client = redis.createClient({
+    url: redis_url,
+  });
+
+  const getAsync = promisify(client.get).bind(client);
+  const setAsync = promisify(client.set).bind(client);
+
+  getEarthquakes = async (key) => {
+    let earthquakes = await getAsync(key);
+    return earthquakes;
+  }
+
+  setEarthquakes = async (key, earthquakes) => {
+    await setAsync(key, earthquakes);
+  }
 }
-
-const client = redis.createClient({
-  url: redis_url,
-});
-
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.set).bind(client);
-
-export async function getEarthquakes(key) {
-  let earthquakes = await getAsync(key);
-  return earthquakes;
-}
-
-export async function setEarthquakes(key, earthquakes) {
-  await setAsync(key, earthquakes);
+catch (e) {
+  console.error('Error setting up redis client, running without cache', e);
+  getEarthquakes = async (key) => { return false};
+  setEarthquakes = async (key, value) => { return }
 }
